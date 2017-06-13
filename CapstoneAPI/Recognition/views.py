5fed16b3f259fa16ea4b79ff63c3bf6523849aa0
Adam Myers <ANMyers53@yahoo.com>
2017-06-13 16:25:16 -0500

    Work in progress for formating labeling of variables

--- CapstoneAPI/Recognition/views.py
+++ CapstoneAPI/Recognition/views.py
@@ -4,6 +4,7 @@ from rest_framework.authtoken.models import Token
 from django.views.decorators.csrf import csrf_exempt
 import json
 from Recognition.models import *
+from sklearn.neighbors import NearestNeighbors
 
 # Create your views here.
 @csrf_exempt
@@ -61,7 +62,10 @@ def format_dataset(request):
         request (POST): Sends data from user's dataset to be analysed 
         
     Returns:     
-        return (dictionary): data from the results of running the algorithm
+        return (json):  "sample_set": one index of the list of datasets to use as an example for which 
+                                indexes to ignore.
+                        "indexs": list of tuples, each tuple has the index of the value, and the 
+                                given value for each non-number value.
     
     Author:
         Adam Myers
@@ -91,8 +95,31 @@ def format_dataset(request):
             reformated_list.append(new_list)
 
     sample_set = reformated_list[0]
+
+    # percentage = percentage_of_numbers(sample_set)
+
+    # if algorithm == 'Nearest Neighbor':
+    #     if percentage > 0.75:
+    #         go_on = True
+    #     else:
+    #         go_on = False
+    # else:
+    #     go_on = True
+    go_on = True
+
     dicts_of_ignored_values = list(dicts_of_ignored_values)
-    data = json.dumps({"sample_set":sample_set, "indexs": dicts_of_ignored_values})
+    data = json.dumps({"sample_set":sample_set, "indexs": dicts_of_ignored_values, "continue": go_on})
     return HttpResponse(data, content_type='application/json')
 
+def percentage_of_numbers(sample):
+    numbers = 0
+    total = 0
+    for each in sample:
+        try:
+            float(each)
+            numbers = numbers + 1
+            total = total + 1
+        except ValueError:
+            total = total + 1
 
+    return numbers / total
--- CapstoneClient/RecognitionClient/app/TryItCtrl.js
+++ CapstoneClient/RecognitionClient/app/TryItCtrl.js
@@ -3,8 +3,12 @@
 app.controller("TryItController", function($scope, $http, $location, RootFactory, apiUrl) {
 
   $scope.algorithms = ['Nearest Neighbor', 'Test'];
+  $scope.step1 = true;
+  $scope.step2 = false;
+  $scope.error = false;
+  $scope.variable = [];
 
-  let validate_compatibilty = function() {
+  let format_dataset = function() {
     $http({
     url: `${apiUrl}/format_dataset/`,
     method: "POST",
@@ -18,30 +22,46 @@ app.controller("TryItController", function($scope, $http, $location, RootFactory
   }).then(
     res => {
       console.log("Data: ", res.data);
+      if (res.data['continue']) {
+      $scope.step1 = false;
+      $scope.step2 = true;
+      $scope.error = false;
+      $scope.variables = res.data.indexs;
+      $scope.sample_set = res.data.sample_set;
+      } else {
+      $scope.error = true;
+      $scope.error_message = "Please Choose a different dataset to match your algorithm.";
+      }
     },
     err => {
       console.log("Error: ", err);
     });
   };
 
-  $scope.test = function(){
+  $scope.step_1 = function(){
     var f = document.getElementById('dataset').files[0],
         r = new FileReader();
     r.onloadend = function(e){
       $scope.dataset = e.target.result;
       if ($scope.SelectedAlgo) {
-      validate_compatibilty();
+      format_dataset();
       } else {
         // This needs to update the html page (through binding) to show the user what they must do
-        console.log("Please Choose an Algorithm.");
+        $scope.error = true;
+        $scope.error_message = 'Please Choose an Algorithm.';
       }
     };
     if (f) {
       r.readAsBinaryString(f);
     } else {
         // This needs to update the html page (through binding) to show the user what they must do
-        console.log("Please Upload a Dataset.");
+        $scope.error = true;
+        $scope.error_message = 'Please Upload a Dataset.';
     }
+  };
+
+  $scope.step_2 = function() {
+
   };
 
 });
